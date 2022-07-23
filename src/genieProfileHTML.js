3ee04d063bf5dae5567724879aadc03f86f6b610
Jason <el.jsteer@gmail.com>
2022-07-23 19:35:06 +1000

    updated email and html formatting

--- src/genieProfileHTML.js
+++ src/genieProfileHTML.js
@@ -6,59 +6,64 @@ const Intern = require("../lib/Intern");
 
 
 const generateManagerCard = (teamMember) =>
-  `<div class="card">
+  `
+  <div class="card">
     <div class="card-header" style="background-color: rgb(1, 96, 139); color: white;">
-      <h5 class="card-title">${team[i].Name}</h5>
-      <h6 class="card-title"><i class="fa-regular fa-mug-hot"></i>${team[i]}</h6>
+      <h5 class="card-title">${teamMember.Name}</h5>
+      <h6 class="card-title"><i class="fa-regular fa-mug-hot"></i>${teamMember}</h6>
     </div>
   <div class="card-body">
     <ul class="list-group">
-      <li class="list-group-item">ID: ${team[i].id}</li>
-      <li class="list-group-item" href="mailto:${team[i].email}">Email: ${team[i].email}</li>
-      <li class="list-group-item">Office Number: ${team[i].mngrOfficeNum}</li>
+      <li class="list-group-item">ID: ${teamMember.id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${teamMember.email}">${teamMember.email}</a></li>
+      <li class="list-group-item">Office Number: ${teamMember.mngrOfficeNum}</li>
     </ul>
   </div>`;
 
 const generateEngineerCard = (teamMember) =>
-  `<div class="card">
+  `
+  <div class="card">
     <div class="card-header" style="background-color: rgb(1, 96, 139); color: white;">
-      <h5 class="card-title">${team[i].Name}</h5>
-      <h6 class="card-title"><i class="fa-regular fa-laptop-code"></i> ${team[i]}</h6>
+      <h5 class="card-title">${teamMember.Name}</h5>
+      <h6 class="card-title"><i class="fa-regular fa-laptop-code"></i>${teamMember}</h6>
     </div>
   <div class="card-body">
     <ul class="list-group">
-      <li class="list-group-item">ID: ${team[i].id}</li>
-      <li class="list-group-item" href="mailto:${team[i].email}">Email: ${team[i].email}</li>
-      <li class="list-group-item" href="https://github.com/${team[i].engrGitHub}">Github: ${team[i].engrGitHub}</li>
+      <li class="list-group-item">ID: ${teamMember.id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${teamMember.email}">${teamMember.email}</a></li>
+      <li class="list-group-item" href="https://github.com/${teamMember.engrGitHub}">Github: ${teamMember.engrGitHub}</li>
     </ul>
   </div>`;
 
 const generateInternCard = (teamMember) =>
-  `<div class="card">
+  `
+  <div class="card">
     <div class="card-header" style="background-color: rgb(1, 96, 139); color: white;">
-      <h5 class="card-title">${team[i].Name}</h5>
-      <h6 class="card-title"><i class="fa-regular fa-user-graduate"></i>${team[i]}</h6>
+      <h5 class="card-title">${teamMember.Name}</h5>
+      <h6 class="card-title"><i class="fa-regular fa-user-graduate"></i>${teamMember}</h6>
     </div>
   <div class="card-body">
     <ul class="list-group">
-      <li class="list-group-item">ID: ${team[i].id}</li>
-      <li class="list-group-item" href="mailto:${team[i].email}">Email: ${team[i].email}</li>
-      <li class="list-group-item">School: ${team[i].intrnSchool}</li>
+      <li class="list-group-item">ID: ${teamMember.id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${teamMember.email}">${teamMember.email}</a></li>
+      <li class="list-group-item">School: ${teamMember.intrnSchool}</li>
     </ul>
   </div>`;
 
 function generateEmployeeCards (team) {
+  let html = "";
   for (i = 0; i < team.length; i++) {
     if(team[i] instanceof Manager) {
-      generateManagerCard();
+      html = html.concat(generateManagerCard(team[i]));
     }
     else if(team[i] instanceof Engineer) {
-      generateEngineerCard();
+      html = html.concat(generateEngineerCard(team[i]));
     }
     else if(team[i] instanceof Intern) {
-      generateInternCard();
+      html = html.concat(generateInternCard(team[i]));
     };
   };
+  return html;
 };
 
 const genieProfileHTML = (team) =>
@@ -73,7 +78,7 @@ const genieProfileHTML = (team) =>
 <body>
   <div class="card col-sm-6 col-md-4 col-9 col-lg-3 m-3">
     <div class="card-deck">
-      ${generateEmployeeCards(team[i])}
+      ${generateEmployeeCards(team)}
     </div>
   </div>
   <script src="https://kit.fontawesome.com/f271dd3923.js" crossorigin="anonymous"></script>
